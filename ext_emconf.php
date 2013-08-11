15f422bcca59021a0c1a11671c3dcf5b80ddf63d
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-11 20:37:53 +0000

    Set Extension state to beta
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yag_theme_magnificpopup/trunk@78959 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -37,6 +37,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' =>
 		array (
 			'typo3' => '6.0.0-6.1.99',
+			'yag' => ''
 		),
 		'conflicts' => '',
 		'suggests' =>
