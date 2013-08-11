057b7cd0f11e3bbef53c5b5dda176c23bb608daa
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-11 20:53:47 +0000

    Updated ext_emconf.php after release of version 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yag_theme_magnificpopup/trunk@78961 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,20 +3,20 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "yag_theme_magnificpopup".
  *
- * Auto generated 07-08-2013 21:53
+ * Auto generated 11-08-2013 22:53
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'YAG Theme Magnific Popup',
 	'description' => 'Theme for YAG gallery that integrates Magnific Popup for displaying images.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.0.1',
-	'dependencies' => '',
+	'version' => '0.1.0',
+	'dependencies' => 'yag',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -30,20 +30,21 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
 	'author_company' => 'Webprogrammierung Jonathan Heilmann',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
 			'typo3' => '6.0.0-6.1.99',
-			'yag' => ''
+			'yag' => '',
 		),
-		'conflicts' => '',
-		'suggests' =>
-		array (
+		'conflicts' => array(
+		),
+		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:35:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"3927";s:10:"Readme.rst";s:4:"f13b";s:38:"Configuration/TypoScript/constants.txt";s:4:"93b9";s:34:"Configuration/TypoScript/setup.txt";s:4:"2490";s:37:"Documentation/_IncludedDirectives.rst";s:4:"53e5";s:37:"Documentation/AdministratorManual.rst";s:4:"2562";s:33:"Documentation/DeveloperCorner.rst";s:4:"e805";s:23:"Documentation/Index.rst";s:4:"3cbc";s:36:"Documentation/ProjectInformation.rst";s:4:"152a";s:38:"Documentation/RestructuredtextHelp.rst";s:4:"c86e";s:37:"Documentation/TyposcriptReference.rst";s:4:"5829";s:28:"Documentation/UserManual.rst";s:4:"3308";s:44:"Documentation/Images/IntroductionPackage.png";s:4:"cdeb";s:30:"Documentation/Images/Typo3.png";s:4:"4fac";s:61:"Documentation/Images/AdministratorManual/ExtensionManager.png";s:4:"14fc";s:47:"Documentation/Images/UserManual/BackendView.png";s:4:"ba6c";s:32:"Documentation/_De/UserManual.rst";s:4:"82b7";s:51:"Documentation/_De/Images/UserManual/BackendView.png";s:4:"ba6c";s:32:"Documentation/_Fr/UserManual.rst";s:4:"56b8";s:51:"Documentation/_Fr/Images/UserManual/BackendView.png";s:4:"ba6c";s:40:"Resources/Private/Language/locallang.xml";s:4:"a0bc";s:42:"Resources/Private/Partials/AlbumThumb.html";s:4:"de98";s:44:"Resources/Private/Partials/GalleryThumb.html";s:4:"f76c";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"f0ae";s:43:"Resources/Private/Templates/Album/List.html";s:4:"f2af";s:46:"Resources/Private/Templates/Gallery/Index.html";s:4:"c2f7";s:45:"Resources/Private/Templates/Gallery/List.html";s:4:"9bca";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"b4db";s:39:"Resources/Public/CSS/magnific-popup.css";s:4:"6bfa";s:31:"Resources/Public/CSS/styles.css";s:4:"9e17";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:48:"Resources/Public/JavaScript/jquery-1.10.2.min.js";s:4:"841d";s:56:"Resources/Public/JavaScript/jquery.magnific-popup.min.js";s:4:"3db9";s:14:"doc/manual.sxw";s:4:"cc00";}',
 );
 
 ?>
