2f7c1e20d81363b0ed96850b17c6c27932c7a04a
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-19 15:05:00 +0000

    Fixed Bug that disabled Magnific Popup for EXT:yag Plugin Mode 'Specific Album' and release of extension version 0.1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yag_theme_magnificpopup/trunk@79041 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Theme for YAG gallery that integrates Magnific Popup for displaying images.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'dependencies' => 'yag',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:35:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"3927";s:10:"Readme.rst";s:4:"f13b";s:38:"Configuration/TypoScript/constants.txt";s:4:"93b9";s:34:"Configuration/TypoScript/setup.txt";s:4:"2490";s:37:"Documentation/_IncludedDirectives.rst";s:4:"53e5";s:37:"Documentation/AdministratorManual.rst";s:4:"2562";s:33:"Documentation/DeveloperCorner.rst";s:4:"e805";s:23:"Documentation/Index.rst";s:4:"3cbc";s:36:"Documentation/ProjectInformation.rst";s:4:"152a";s:38:"Documentation/RestructuredtextHelp.rst";s:4:"c86e";s:37:"Documentation/TyposcriptReference.rst";s:4:"5829";s:28:"Documentation/UserManual.rst";s:4:"3308";s:44:"Documentation/Images/IntroductionPackage.png";s:4:"cdeb";s:30:"Documentation/Images/Typo3.png";s:4:"4fac";s:61:"Documentation/Images/AdministratorManual/ExtensionManager.png";s:4:"14fc";s:47:"Documentation/Images/UserManual/BackendView.png";s:4:"ba6c";s:32:"Documentation/_De/UserManual.rst";s:4:"82b7";s:51:"Documentation/_De/Images/UserManual/BackendView.png";s:4:"ba6c";s:32:"Documentation/_Fr/UserManual.rst";s:4:"56b8";s:51:"Documentation/_Fr/Images/UserManual/BackendView.png";s:4:"ba6c";s:40:"Resources/Private/Language/locallang.xml";s:4:"a0bc";s:42:"Resources/Private/Partials/AlbumThumb.html";s:4:"de98";s:44:"Resources/Private/Partials/GalleryThumb.html";s:4:"f76c";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"f0ae";s:43:"Resources/Private/Templates/Album/List.html";s:4:"f2af";s:46:"Resources/Private/Templates/Gallery/Index.html";s:4:"c2f7";s:45:"Resources/Private/Templates/Gallery/List.html";s:4:"9bca";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"b4db";s:39:"Resources/Public/CSS/magnific-popup.css";s:4:"6bfa";s:31:"Resources/Public/CSS/styles.css";s:4:"9e17";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:48:"Resources/Public/JavaScript/jquery-1.10.2.min.js";s:4:"841d";s:56:"Resources/Public/JavaScript/jquery.magnific-popup.min.js";s:4:"3db9";s:14:"doc/manual.sxw";s:4:"cc00";}',
+	'_md5_values_when_last_written' => '',
 );
 
 ?>
