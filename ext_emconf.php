38fe76b0305070c98e41a8c7602e11ecb8f7f076
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-20 20:31:48 +0000

    Updated dependencies again
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yag_theme_magnificpopup/trunk@81674 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,48 +3,38 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "yag_theme_magnificpopup".
  *
- * Auto generated 20-11-2013 21:08
+ * Auto generated 20-11-2013 21:25
  *
  * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
 	'title' => 'YAG Theme Magnific Popup',
 	'description' => 'Theme for YAG gallery that integrates Magnific Popup for displaying images.',
 	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '0.1.3',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
 	'author_company' => 'Webprogrammierung Jonathan Heilmann',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
-			'typo3' => '4.5.0-6.1.99',
-			'yag' => '',
-
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => '0',
+	'lockType' => '',
+	'version' => '0.1.3',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-6.1.99',
 		),
-		'conflicts' =>
-		array (
+		'conflicts' => array(
 		),
-		'suggests' =>
-		array (
+		'suggests' => array(
 		),
 	),
 );
